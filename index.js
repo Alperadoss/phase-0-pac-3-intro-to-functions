52d250e9ec2c67020d3b58a4872654efa9f15e29
Alper Yazagan <alperyazagan@gmail.com>
2023-03-13 16:46:47 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Follow along with the examples here
+function say(greetings, name) {
+  console.log(`${greetings} ${name}!`);
+}
+
+//say("hello", "Guadalupe");
+//say("goodbye", "Liz");
+//say("hi", "Samip");
+
+function add(x, y) {
+  return x + y;
+}
+console.log(add(1, 2));
